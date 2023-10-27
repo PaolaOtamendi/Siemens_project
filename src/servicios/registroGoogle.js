fd63c8bcf661cbaaf147c17fc60bd4a8c1143847
PaolaOtamendi <paola_otamendi23@hotmail.com>
2023-10-27 16:20:43 -0600

    feat cambios css footer

--- src/servicios/registroGoogle.js
+++
@@ -1,8 +0,0 @@
-/* import { signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
-
-const auth = getAuth();
-const provider = new GoogleAuthProvider();
-
-export const signInWithGoogle = () => {
-    return signInWithPopup(auth, provider);
-} */
